aa4d61b9c7bf7879863f4ec2a6c6eb37237359c5
大大只的细细粒 <again9527@outlook.com>
2025-07-10 11:00:04 +0800

    确保数据库能正常断开链接

--- Excel_Toolkit.py
+++ Excel_Toolkit.py
@@ -1,17 +1,22 @@
 import webview
 from SQLite import SQLite
 
-
 if __name__ == "__main__":
-    
-    win = webview.create_window(
-        title="Excel Toolkit",
-        url="Excel_Toolkit.html",
-        text_select=True,
-        width=1000,
-        height=800,
-        js_api=SQLite(),
-    )
-    
-    webview.settings["OPEN_DEVTOOLS_IN_DEBUG"] = False
-    webview.start(debug=True)
+    try :
+        api = SQLite()
+        win = webview.create_window(
+            title="Excel Toolkit",
+            url="Excel_Toolkit.html",
+            text_select=True,
+            width=1000,
+            height=800,
+            js_api=api,
+        )
+
+        webview.settings["OPEN_DEVTOOLS_IN_DEBUG"] = False
+        webview.start(debug=True)
+    except Exception as e:
+        print(f"发生未知错误: {e}")
+    finally:
+        if api:
+            api.close()
